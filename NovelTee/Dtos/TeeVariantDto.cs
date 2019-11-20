c72e87de560f6e0eaef16ab474faa30449f31d03
Mitch-SB <mitch_stbrmdz@hotmail.com>
2019-11-20 01:31:44 -0500

    Add Dtos for Color, Gender, and Size

---
+++ NovelTee/Dtos/TeeVariantDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NovelTee.Dtos
+{
+    public class TeeVariantDto
+    {
+        public int Id { get; set; }
+                
+        [Required]
+        public byte ColorId { get; set; }
+        
+        [Required]
+        public byte SizeId { get; set; }
+        
+        [Required]
+        public byte GenderId { get; set; }
+
+        public byte Quantity { get; set; }
+    }
+}
