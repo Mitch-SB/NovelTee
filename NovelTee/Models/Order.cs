75a5e33c4a88239cc7b1cb0901c60db2457656cb
Mitch-SB <mitch_stbrmdz@hotmail.com>
2020-08-11 00:48:19 -0400

    Add Order and OrderDetails models

---
+++ NovelTee/Models/Order.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NovelTee.Models
+{
+    public class Order
+    {        
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage ="Please enter your first name")]
+        [Display(Name ="First Name")]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address")]
+        [Display(Name = "Street Address")]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [StringLength(25)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter your state")]
+        [StringLength(2, MinimumLength = 2)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your zip")]
+        [Display(Name = "Zip Code")]
+        [StringLength(5, MinimumLength = 5)]
+        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone nummber")]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        
+        public decimal OrderTotal { get; set; }
+
+
+        public DateTime OrderPlaced { get; set; }
+
+    }
+}
---
+++ NovelTee/Models/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NovelTee.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int VariantId { get; set; }
+        public TeeVariant TeeVariant { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public Order Order { get; set; }
+    }
+}
